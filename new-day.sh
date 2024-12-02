a689564c6a3f39ec05c5dd7f4d05d71b7db5fa48
Johannes Eickhold <jeick@innoopract.com>
2024-12-03 00:26:34 +0100

    🐛 Don't pad single digit day directories with 0
    Downloads don't work if single digit day of month is padded
    with `0`.
    
    Use colored output.

--- new-day.sh
+++ new-day.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/bash
 
+function green() {
+  echo "\033[0;32m$1\033[0m"
+}
+
+function red() {
+  echo "\033[0;31m$1\033[0m"
+}
+
 AOC_SESSION_COOKIE=$(cat aoc-session-cookie.txt)
 
-# year=$(date +%Y)
-year=2023
+if [ -z "$1" ]; then
+  year=$(date +%Y)
+else
+  year=$1
+fi
+if [ -z "$2" ]; then
+  dayOfMonth=$(date +%-d)
+else
+  dayOfMonth=$2
+fi
 today=$(date +%m)
-# dayOfMonth=$(date +%d)
-dayOfMonth=1
 
 if [ "$today" -ne 12 ]; then
   echo "It's not yet December, the month of AoC."
@@ -16,12 +30,12 @@ fi
 dayDir="$dayOfMonth"
 
 if [ ! -d "$dayDir" ]; then
-  echo "create missing directory for AoC day $dayOfMonth..."
+  echo -e "create missing directory for AoC day $(green $dayOfMonth) ..."
   mkdir "$dayOfMonth"
-  echo "initializing day $dayOfMonth with template..."
+  echo -e "initializing day $(green $dayOfMonth) with template ..."
   cp -r template/* "$dayOfMonth"
-  echo "fetching data of day $dayOfMonth..."
-  curl -o "$dayOfMonth/data/data.txt" "https://adventofcode.com/$year/day/$dayOfMonth/input" --cookie "$AOC_SESSION_COOKIE"
 else
-  echo "directory for day $dayDir already exists"
+  echo -e "directory for day $(red $dayDir) already exists"
 fi
+echo -e "fetching data of day $(green $dayOfMonth) ..."
+curl -o "$dayOfMonth/data/data.txt" "https://adventofcode.com/$year/day/$dayOfMonth/input" --cookie "$AOC_SESSION_COOKIE"
